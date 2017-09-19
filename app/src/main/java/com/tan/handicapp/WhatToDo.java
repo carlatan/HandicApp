3f3a9c155b0ea92211c4118a4f25c0abbd11e989
Carla <tanfamily@God-loves-you.local>
2017-09-19 23:29:50 +0800

    Initial commit

---
+++ app/src/main/java/com/tan/handicapp/WhatToDo.java
@@ -0,0 +1,13 @@
+package com.tan.handicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WhatToDo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_what_to_do);
+    }
+}
